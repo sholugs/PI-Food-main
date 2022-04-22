5c7d2eb2d5ea87894a5332d87d8dd878cae40bb2
Gabriel Cavasso <gaerca123@gmail.com>
2022-04-22 19:13:21 -0300

    Initial commit

---
+++ api/src/models/Recipe.js
@@ -0,0 +1,22 @@
+const { DataTypes, UUIDV4 } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('recipe', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      defaultValue: UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+
+  });
+};
+
+
